3bfa583ccccd07bfe7416842888cc77a7a052af1
AgentSMDJ <sandermae036@gmail.com>
2026-09-17 13:50:58 +0200

    added header and main imports to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,8 @@
 // imports ------------------------------------------------------------------------------------------------------
+import {header} from "../components/htmlElement.js";
+
+import {main} from "../components/htmlElement.js";
+
 import {h1} from "../components/htmlElement.js";
 
 import {h2} from "../components/htmlElement.js";
@@ -15,6 +19,22 @@ import {ul} from "../components/htmlElement.js";
 
 import {li} from "../components/htmlElement.js";
 
+
+
+
+
+
+
+
+
+
 // html inserts -------------------------------------------------------------------------------------------------
 const root = document.querySelector("#root");
 
+
+// make an img with pokeball img as src
+
+
+// make an h1 with pok&eacute;dex txt
+
+// make a section
