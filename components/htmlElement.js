93d8111f290f0ad9648a71a787c393acd19e8622
AgentSMDJ <sandermae036@gmail.com>
2026-09-17 12:51:13 +0200

    added the files and folders

---
+++ components/htmlElement.js
@@ -0,0 +1,5 @@
+export function h1 (text) {
+    let headline = document.createElement("h1");
+    headline.textContent = text;
+    return headline
+}
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+import {h1} from "../components/htmlElement.js";
+
+const root = document.querySelector("#root");
+
+root.append(h1("test"))
