dfdfc5bb82392ac400b66d9f27909cf87536ecfa
AgentSMDJ <sandermae036@gmail.com>
2026-09-18 10:43:09 +0200

    added the final style for headline

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 // imports ------------------------------------------------------------------------------------------------------
-import {header} from "../components/htmlElement.js";
+import { header } from "../components/htmlElement.js";
 
-import {main} from "../components/htmlElement.js";
+import { main } from "../components/htmlElement.js";
 
-import {h1} from "../components/htmlElement.js";
+import { h1 } from "../components/htmlElement.js";
 
-import {h2} from "../components/htmlElement.js";
+import { h2 } from "../components/htmlElement.js";
 
-import {p} from "../components/htmlElement.js";
+import { p } from "../components/htmlElement.js";
 
-import {img} from "../components/htmlElement.js";
+import { img } from "../components/htmlElement.js";
 
-import {article} from "../components/htmlElement.js";
+import { article } from "../components/htmlElement.js";
 
-import {section} from "../components/htmlElement.js";
+import { section } from "../components/htmlElement.js";
 
-import {div} from "../components/htmlElement.js";
+import { div } from "../components/htmlElement.js";
 
-import {ul} from "../components/htmlElement.js";
+import { ul } from "../components/htmlElement.js";
 
-import {li} from "../components/htmlElement.js";
+import { li } from "../components/htmlElement.js";
 
 
 
@@ -39,25 +39,25 @@ root.append(header())
 const headerElement = document.querySelector("header");
 
 
-    // make div and append in header
-    headerElement.append(div("imgHolder"));
+// make div and append in header
+headerElement.append(div("imgHolder"));
 
 
-        const imgDiv = document.querySelector("#imgHolder");
-
-
-
-            // make an img with pokeball img as src and append in div 
-            imgDiv.append(img("../img/Pokeball.png", "pokeball"))
-    
-
-
-        // make an h1 with pok&eacute;dex txt
-        headerElement.append(h1("Pokedex"))
+const imgDiv = document.querySelector("#imgHolder");
 
 
 
-    // search --------------------------------------------------
+// make an img with pokeball img as src and append in div 
+imgDiv.append(img("../img/Pokeball.png", "pokeball"))
+
+
+
+// make an h1 with pok&eacute;dex txt
+headerElement.append(h1("Pokédex"))
+
+
+
+// search --------------------------------------------------
 
 
 
