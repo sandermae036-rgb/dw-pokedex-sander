3d955dda5b1762cef4913a55b2218c353aaca116
AgentSMDJ <sandermae036@gmail.com>
2026-09-18 10:17:13 +0200

    added js to make the header done

--- scripts/index.js
+++ scripts/index.js
@@ -15,6 +15,8 @@ import {article} from "../components/htmlElement.js";
 
 import {section} from "../components/htmlElement.js";
 
+import {div} from "../components/htmlElement.js";
+
 import {ul} from "../components/htmlElement.js";
 
 import {li} from "../components/htmlElement.js";
@@ -31,10 +33,35 @@ import {li} from "../components/htmlElement.js";
 // html inserts -------------------------------------------------------------------------------------------------
 const root = document.querySelector("#root");
 
+// header ------------------------------------------------------------------------------
+// make header and append in root 
+root.append(header())
+const headerElement = document.querySelector("header");
 
-// make an img with pokeball img as src
+
+    // make div and append in header
+    headerElement.append(div("imgHolder"));
 
 
-// make an h1 with pok&eacute;dex txt
+        const imgDiv = document.querySelector("#imgHolder");
 
-// make a section
+
+
+            // make an img with pokeball img as src and append in div 
+            imgDiv.append(img("../img/Pokeball.png", "pokeball"))
+    
+
+
+        // make an h1 with pok&eacute;dex txt
+        headerElement.append(h1("Pokedex"))
+
+
+
+    // search --------------------------------------------------
+
+
+
+// main / pokeindex ---------------------------------------------------------------------------
+root.append(main())
+const mainDOM = document.querySelector("main");
+
